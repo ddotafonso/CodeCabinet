a4e573fd9153b0c1b63b61f2e90f60b57a318781
DKafonso <DKafonso@outlook.com>
2021-03-17 09:09:10 +0000

    merging python lists in O(n+m) and O(n) space complexity

---
+++ mergingpythonlists.py
@@ -0,0 +1,16 @@
+# Linking two lists together in O(n+m) complexity
+
+def sortingLists(arr1, arr2):
+    sortednumbers = []
+    for num1 in numbers1:
+        sortednumbers.append(num1)
+    
+    for num2 in numbers2:
+            sortednumbers.append(num2)
+    
+    return sortednumbers
+
+
+numbers1 = [2,5,7,8,9,1]
+numbers2 = []
+print(sortingLists(numbers1, numbers2))
