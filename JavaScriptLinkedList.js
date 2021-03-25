558a41e47200554101c587e57e9a6cac87156007
DKafonso <DKafonso@outlook.com>
2021-03-25 19:17:12 +0000

    JavaScript implementation of a Linked List

---
+++ JavaScriptLinkedList.js
@@ -0,0 +1,28 @@
+class LinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null
+        }
+        this.tail = this.head
+        this.length = 1;
+    }
+
+    append(value) {
+        const newNode = {
+            value: value,
+            next: null
+        };
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+
+}
+
+const myLinkedList = new LinkedList(590);
+myLinkedList.append(5);
+myLinkedList.append(16);
+// myLinkedList.prepend(800);
+console.log(myLinkedList)
