76cda023981c23ca29c96b911dbae7a75c9c14bf
DKafonso <DKafonso@outlook.com>
2021-04-04 11:35:36 +0100

    leisure time: implementing a doubly linked list and a simple menu manager

--- menuManager/menumanager.py
+++ menuManager/menumanager.py
@@ -6,35 +6,47 @@ class Manager:
     def get_name(self):
         print(f"Welcome to" + " " + self.name)
         
-    # This function prints only the categories name
-    def print_onlycategories(self, categories):
-        for categ_name, categ_info in categories.items():
+    # This function prints only the categories names
+    def print_onlycategories(self, menu_list):
+        for categ_name, categ_info in menu_list.items():
                 print('Category:',categ_name)
     
     # Exercise A: This function prints all categories including their menu item and price
-    def print_allcategoriesmenu(self, menu):
-        formatted = json.dumps(menu, indent=4)
+    def print_allcategoriesmenuitems(self, menu_list):
+        formatted = json.dumps(menu_list, indent=4)
         print(formatted)
         
     # Exercise D: Prints by specifying a category
-    def list_bycategory(self, category, categories_list):
-        for categ_name, categ_info in categories_list.items():
+    def list_bycategory(self, category, menu_list):
+        for categ_name, categ_info in menu_list.items():
             if category == categ_name:
                 formatted = json.dumps(categ_info, indent=4)
                 print(formatted)
         
     # Exercise B: This function adds an Item in any of the categories
-    def add_item(self, categories, category_name, item , item_price):
-            for categ_name, categ_info in categories.items():
-                categories[category_name][item] = item_price
+    def add_item(self, menu_list, category_name, item , item_price):
+            for categ_name, categ_info in menu_list.items():
+                menu_list[category_name][item] = item_price
+                print(menu_list)
             
     # Exercise D, E: Activating and deactivating a menu item
-    def inactivate_activate(self, categories_list, item, menu):
-        for categ_name, categ_info in categories_list.copy().items():
+    def inactivate_activate(self, menu_list, item, new_menu):
+        for categ_name, categ_info in menu_list.copy().items():
             for key, value in categ_info.items():
                 if item == key:
-                    menu[categ_name][item] = value
-                    del categories_list[categ_name]
+                    new_menu[categ_name][item] = value
+                    del menu_list[item]
+                    print(new_menu, menu_list)
+                    
+    def remove_menu_item(self, item, menu_list):
+        menu_name = 'active_menu'
+        if menu_list == menu_name:
+            print(f'You cannot remove from the active menu, only from inactive')
+        else:
+            for categ_name, categ_info in menu_list.items():
+                if item in categ_info:
+                    del menu_list[categ_name][item]
+                    print(menu_list)
                 
                 
                         
@@ -50,7 +62,7 @@ inactive_menu = {
     }
 }
             
-categories = {
+active_menu = {
     'Hot Drinks':{
         'Hot chocolate': '5', 
         'Cappucino': '3', 
@@ -70,19 +82,5 @@ categories = {
 }
 
 
-
-
-# colds['Cold Drinks']['Iced Tea'] = '5'
-# print(hots)
 kfc = Manager("KFC")
-# # kfc.getName()
-# kfc.add_item(categories, 'Hot Drinks', 'Muled whine', 10)
-# kfc.add_item(categories, 'Hot Drinks', 'whine', 10)
-# kfc.add_item(categories, 'Hot Drinks', 'Tea', 10)
-# kfc.print_onlycategories(categories)
-# kfc.inactivate_item(categories, 'Hot dog', inactive_menu)
-# kfc.inactivate_item(categories, 'Cake', inactive_menu)
-# kfc.inactivate_item(categories, 'Pizza', inactive_menu)
-# kfc.print_allcategoriesmenu(categories)
-kfc.list_bycategory('Fast Foods', categories)
-# print(categories)
+kfc.remove_menu_item('Brownie', active_menu)
