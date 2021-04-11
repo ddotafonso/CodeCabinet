8fc21123ce75084e0789dd070fc556cd7266bf4b
DKafonso <DKafonso@outlook.com>
2021-04-11 18:22:29 +0100

    adding pop method to stack implementation

--- DSA/stacks.js
+++ DSA/stacks.js
@@ -20,11 +20,39 @@ class Stack {
     }
 
     push(value) {
-        this.value = this.top
+        // this.value = this.top
+        // this.length++;
+        const newNode = new Node(value)
+            // If stack is empty and this is the very new item, top and bottom will be the same item.
+        if (this.length === 0) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const holdingPointer = this.top;
+            this.top = newNode;
+            this.top.next = holdingPointer;
+        }
         this.length++;
+        return this;
     }
-    pop() {}
+    pop() {
+        if (!this.top) {
+            return null;
+        }
+        // Javascript being a garbage collector language, it automatically deletes unwantedNode var since it's not being used.
+        const unwantedNode = this.top;
+        this.top = this.top.next;
+        this.length--;
+        return this;
+    }
     isEmpty() {
         return this.length == 0;
     }
 }
+
+
+const myStack = new Stack();
+myStack.push('google');
+myStack.push('instagram');
+myStack.push('twitter');
+myStack.push('facebook');
