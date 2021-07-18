8647ce34c9b9a1b2af831916d855685d6d92c5fd
DKafonso <DKafonso@outlook.com>
2021-07-18 17:45:49 +0100

    Implementing insert for Binary search tree

--- DSA/binarySearchTreeImpl.js
+++ DSA/binarySearchTreeImpl.js
@@ -11,7 +11,30 @@ class BinarySearchTree {
         this.root = null;
     }
     insert(value) {
-        //Code here
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            let currentNode = this.root;
+            while (true) {
+                if (value < currentNode.value) {
+                    // Left
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    // Right
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+
     }
     lookup(value) {
             //Code here
